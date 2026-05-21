786a3a642811d30f083706d5d1eeb205609d04d2
Ray Jose <162737338+rayajose@users.noreply.github.com>
2026-05-21 12:09:28 -0400

    Update orders.py

--- routers/orders.py
+++ routers/orders.py
@@ -253,11 +253,9 @@ def list_orders():
 
     try:
         rows = cur.execute(q("""
-                SELECT
-                    order_id,
-                    created_at
+                SELECT order_id
                 FROM orders
-                ORDER BY orders.created_at DESC, orders.order_id DESC
+                ORDER BY created_at DESC, order_id DESC
             """)).fetchall()
 
         orders = []
